f01bdf704debd95e9831bfdfcf670b3c37b5672f
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-05-20 15:41:46 +0530

    Bottom call/chat button added

--- lib/profile/ui/profile_page.dart
+++ lib/profile/ui/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:himachali_rishta/core/app_colors.dart';
 import 'package:himachali_rishta/profile/ui/AboutPage.dart';
 import 'package:sizer/sizer.dart';
 
@@ -10,44 +11,75 @@ class ProfilePage extends StatelessWidget {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-            appBar: PreferredSize(
-              preferredSize: Size.fromHeight(0),
-              child: Container(
-                color: Theme.of(context).primaryColor,
-              ),
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(0),
+            child: Container(
+              color: Theme.of(context).primaryColor,
             ),
-            body: Column(
-              children: [
-                TabBar(tabs: [
-                  Tab(
-                    child: Text(
-                      'About',
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 16.sp),
-                    ),
-
-                  ),
-                  Tab(
-                    child: Text(
-                      'Partner Preference',
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 13.sp),
-                    ),
-                  ),
-                ]),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      AboutPage(),
-                      Center(
-                        child: Text('Partner Preference'),
-                      ),
-                    ],
+          ),
+          body: Column(
+            children: [
+              TabBar(tabs: [
+                Tab(
+                  child: Text(
+                    'About',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor, fontSize: 16.sp),
                   ),
                 ),
-              ],
-            )));
+                Tab(
+                  child: Text(
+                    'Partner Preference',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor, fontSize: 13.sp),
+                  ),
+                ),
+              ]),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    AboutPage(),
+                    Center(
+                      child: Text('Partner Preference'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 32.sp, vertical: 8.sp),
+
+                  child: MaterialButton(
+                    onPressed: () {},
+                    color: AppColors.alternate,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0.sp),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.call,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Call/Chat',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    height: 38.sp,
+                    minWidth: 138.sp,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
