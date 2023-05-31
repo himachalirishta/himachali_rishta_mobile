273b8b3fd04b536e8ed96e904612eca23fd01f1e
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-05-31 13:02:00 +0530

    API working

--- lib/features/dashboard/get_controllers/all_tab_get_controller.dart
+++ lib/features/dashboard/get_controllers/all_tab_get_controller.dart
@@ -10,21 +10,21 @@ class AllTabGetController extends GetxController {
 
   Future<LatestProfileModel> getLatestProfiles() async {
     var headers = {
-      'Authorization':
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rldm1hdHJpLnJpc2h0YWd1cnUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjg1NDQ4OTI1LCJleHAiOjE2ODU0NTI1MjUsIm5iZiI6MTY4NTQ0ODkyNSwianRpIjoicDNod0xLbUFpSEhMSlVWVCIsInN1YiI6IjExMTMxOSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.xIC5Kb4uqrFg2d-liyED_Jvl0sBjBRunLbo-T2I0-MU'
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rldm1hdHJpLnJpc2h0YWd1cnUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjg1NTE3NTQ5LCJleHAiOjE2ODU1MjExNDksIm5iZiI6MTY4NTUxNzU0OSwianRpIjoiQXlYME1lWUxjUUJ1ZTNCUSIsInN1YiI6IjExMTMxOSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.gjCPgCUN_js81VT8Kq-IPDJ8GqLsG9ZHlSoN3JcPAlc'
     };
-    var request = http.Request(
-        'GET', Uri.parse('https://devmatri.rishtaguru.com/api/latest-profile'));
+    var request = http.Request('GET', Uri.parse('https://devmatri.rishtaguru.com/api/latest-profile'));
 
     request.headers.addAll(headers);
 
     http.StreamedResponse response = await request.send();
-
+    print("Request sent");
     if (response.statusCode == 200) {
+      print("Response received");
       LatestProfileModel latestProfileModel =
           latestProfileModelFromJson(await response.stream.bytesToString());
       return latestProfileModel;
     } else {
+      print(response.reasonPhrase);
       throw response.reasonPhrase.toString();
     }
   }
