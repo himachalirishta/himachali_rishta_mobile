707dfaa70505858c6fe2d99a296274f7dbc54502
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-06-01 11:39:14 +0530

    token problem fixed

--- lib/features/authentication/login/get_controller/login_page_get_controller.dart
+++ lib/features/authentication/login/get_controller/login_page_get_controller.dart
@@ -8,6 +8,7 @@ import 'package:himachali_rishta/features/authentication/login/models/login_requ
 import 'package:himachali_rishta/features/authentication/login/models/login_response.dart';
 import 'package:himachali_rishta/features/authentication/login/ui/OtpScreen.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../ui/SubmitInformationPage.dart';
 
@@ -48,7 +49,11 @@ class LoginPageGetController extends GetxController {
                     .signInWithCredential(credential)
                     .then((value) {
                   initLoginApi().then((value) {
-                    Get.to(() => SubmitInformationPage());
+                    SharedPreferences.getInstance().then((prefs) {
+                      prefs.setString('AccessToken', value.accessToken);
+                    }).then((value) {
+                      Get.to(() => SubmitInformationPage());
+                    });
                   });
                 });
               }));
--- lib/features/dashboard/get_controllers/all_tab_get_controller.dart
+++ lib/features/dashboard/get_controllers/all_tab_get_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:himachali_rishta/features/dashboard/models/latest_profile_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AllTabGetController extends GetxController {
   @override
@@ -9,10 +10,16 @@ class AllTabGetController extends GetxController {
   }
 
   Future<LatestProfileModel> getLatestProfiles() async {
-    var headers = {
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rldm1hdHJpLnJpc2h0YWd1cnUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjg1NTE3NTQ5LCJleHAiOjE2ODU1MjExNDksIm5iZiI6MTY4NTUxNzU0OSwianRpIjoiQXlYME1lWUxjUUJ1ZTNCUSIsInN1YiI6IjExMTMxOSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.gjCPgCUN_js81VT8Kq-IPDJ8GqLsG9ZHlSoN3JcPAlc'
-    };
-    var request = http.Request('GET', Uri.parse('https://devmatri.rishtaguru.com/api/latest-profile'));
+    String token = '';
+    await SharedPreferences.getInstance().then((prefs) {
+      String? accessToken = prefs.getString("AccessToken");
+      if (accessToken != null) {
+        token = accessToken;
+      }
+    });
+    var headers = {'Authorization': 'Bearer $token'};
+    var request = http.Request(
+        'GET', Uri.parse('https://devmatri.rishtaguru.com/api/latest-profile'));
 
     request.headers.addAll(headers);
 
