711391b18ec44cec251da65cd391627abf175fdd
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-07-01 08:03:37 +0530

    Full Profile Page app bar 700 px

--- lib/profile/ui/profile_page.dart
+++ lib/profile/ui/profile_page.dart
@@ -11,111 +11,131 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(0),
-            child: Container(
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 7.h,
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Get.back();
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back,
-                        )),
-                    Text(
-                      "HRP123456",
-                      style: TextStyle(fontSize: 16.sp.adjustedSp),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Image.asset('assets/images/homepagelogo.jpg'),
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.more_vert,
-                                color: Theme.of(context).primaryColor,
-                              ))
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              TabBar(indicatorColor: Theme.of(context).primaryColor, tabs: [
-                Tab(
-                  child: Text(
-                    'About',
-                    style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 16.sp.adjustedSp),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    'Partner Preference',
-                    style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 13.sp.adjustedSp),
-                  ),
-                ),
-              ]),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    AboutPage(),
-                    PartnerPreferenceTab(),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 32.sp.adjustedSp, vertical: 8.sp.adjustedSp),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    color: AppColors.alternate,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4.0.sp.adjustedSp),
-                    ),
-                    height: 38.sp.adjustedSp,
-                    minWidth: 138.sp.adjustedSp,
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.call,
-                          color: Colors.white,
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder<bool>(
+              stream: Stream.periodic(Duration(milliseconds: 100), (_) {
+                return SizerUtil.orientation == Orientation.landscape;
+              }),
+              builder: (context, snapshot) {
+                return SizedBox(
+                  width: snapshot.data! ? 700 : 100.w,
+                  child: DefaultTabController(
+                      length: 2,
+                      child: Scaffold(
+                        appBar: PreferredSize(
+                          preferredSize: const Size.fromHeight(0),
+                          child: Container(
+                            color: Theme.of(context).primaryColor,
+                          ),
                         ),
-                        Text(
-                          'Call/Chat',
-                          style: TextStyle(color: Colors.white),
+                        body: Column(
+                          children: [
+                            SizedBox(
+                              height: 7.h,
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        Get.back();
+                                      },
+                                      icon: const Icon(
+                                        Icons.arrow_back,
+                                      )),
+                                  Text(
+                                    "HRP123456",
+                                    style: TextStyle(fontSize: 16.sp.adjustedSp),
+                                  ),
+                                  Expanded(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Image.asset('assets/images/homepagelogo.jpg'),
+                                        IconButton(
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.more_vert,
+                                              color: Theme.of(context).primaryColor,
+                                            ))
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            TabBar(
+                                indicatorColor: Theme.of(context).primaryColor,
+                                tabs: [
+                                  Tab(
+                                    child: Text(
+                                      'About',
+                                      style: TextStyle(
+                                          color: Theme.of(context).primaryColor,
+                                          fontSize: 16.sp.adjustedSp),
+                                    ),
+                                  ),
+                                  Tab(
+                                    child: Text(
+                                      'Partner Preference',
+                                      style: TextStyle(
+                                          color: Theme.of(context).primaryColor,
+                                          fontSize: 13.sp.adjustedSp),
+                                    ),
+                                  ),
+                                ]),
+                            Expanded(
+                              child: TabBarView(
+                                children: [
+                                  AboutPage(),
+                                  PartnerPreferenceTab(),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
+                        bottomNavigationBar: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 32.sp.adjustedSp,
+                                    vertical: 8.sp.adjustedSp),
+                                child: MaterialButton(
+                                  onPressed: () {},
+                                  color: AppColors.alternate,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.circular(4.0.sp.adjustedSp),
+                                  ),
+                                  height: 38.sp.adjustedSp,
+                                  minWidth: 138.sp.adjustedSp,
+                                  child: const Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.call,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        'Call/Chat',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                );
+              }),
+        ],
+      ),
+    );
     ;
   }
 }
